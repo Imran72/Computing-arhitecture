9768c75f2c2e45a7271e0f5961107d2417bc8abc
imrantimkanov <itimkanov2014@litsey2.ru>
2020-11-30 19:48:18 +0500

    some

--- task04/main.cpp
+++ task04/main.cpp
@@ -7,20 +7,20 @@
 
 using namespace std;
 
-void exam_session(vector<int>);
+void exam_session(vector<int>); //  сигнатура метода сессии
 
-queue<int> studentsCheckList;
+queue<int> studentsCheckList; // очередь студентов сдавших на проверку билет
 
 
 void student_exam(int student) {
-#pragma omp critical
+#pragma omp critical // обязательный процесс в потоке: вывод информации и добавление студентва в очередь
     {
         string b = "\nСтудент #" + to_string(student + 1) +
                    " подготовил ответ на свой билет и отдал его на проверку...\n";
         cout << b;
         studentsCheckList.push(student);
     }
-#pragma omp critical
+#pragma omp critical // обязательный процесс в потоке: вывод информации об оценке студента + удаление информации о нем из очереди
     {
         string a = "\nРабота студента #" + to_string(studentsCheckList.front() + 1) + " была проверена. Оценка " +
                    to_string(rand() % 11) +
@@ -30,7 +30,7 @@ void student_exam(int student) {
     }
 }
 
-bool IsNumber(string s)
+bool IsNumber(string s) // проверка на число
 {
     for (int i = 0; i < s.length(); i++)
         if (isdigit(s[i]) == false)
@@ -41,12 +41,12 @@ bool IsNumber(string s)
 
 int main() {
 
-    setlocale(LC_ALL, "ru");
+    setlocale(LC_ALL, "ru"); // изменение локали
 
     string s; // number of students
 
     cout << "Введите число студентов в аудитории: ";
-    while (cin >> s) {
+    while (cin >> s) { //считывание данных
         if (IsNumber(s) && stoi(s) <= 100 && stoi(s) > 0)
             break;
         cout << "Из-за короновируса в аудиторию может вместитить от 1 до 100 студентов." << endl;
@@ -69,18 +69,18 @@ int main() {
         }
     }
 
-    exam_session(students);
+    exam_session(students); // процесс сессии - запуск метода запускающего потоки
 
     return 0;
 }
 
 
 void exam_session(vector<int> students) {
-#pragma omp parallel for
+#pragma omp parallel for //  создание параллельных потоков через цикл for
     {
         for (int i = 0; i < students.size(); i++) {
-            srand(int(time(NULL)) ^ omp_get_thread_num());
-            student_exam(students[i]);
+            srand(int(time(NULL)) ^ omp_get_thread_num()); // каждый раз задаем рандом, чтобы числа не повторялись
+            student_exam(students[i]); // какой-то студент сдал билет на проверку
         }
     }
 }
